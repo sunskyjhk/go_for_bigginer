13402cdad870ea9249b97de3aaddf1e06aa77b82
genesis.kim <genesis.kim@navercorp.com>
2020-03-30 06:52:45 +0900

    add tutorial files for chapter 09

---
+++ 09_Parsing_JSON_files_With_Golang/parsing_json.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+func main() {
+	// Open our jsonFile
+	jsonFile, err := os.Open("users.json")
+
+	// if we os.Open returns an error then handle it
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Println("Successfully Opened users.json")
+	// defer the closing of our jsonFile so that we can parse it later on
+	defer jsonFile.Close()
+}
